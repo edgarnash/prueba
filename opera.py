0c81354ab28ec3382b014d45a0c05f920dab1a23
Edgar P Nash <edgar.nash@gmail.com>
2021-04-04 00:20:02 -0500

    subiendo otro file

---
+++ opera.py
@@ -0,0 +1,6 @@
+num = 3
+num1 = 4
+num2 = 9
+
+total = num * num1 - num2
+print(total) 
--- suma.py
+++ suma.py
@@ -1,5 +1,5 @@
 num1 = int(input("entre el num1: "))
 num2 = int(input("entre el num2: "))
 
-total = (num1 + num2)
+total = (num1 + num2)/2 
 print(total)
